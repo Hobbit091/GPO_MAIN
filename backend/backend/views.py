297ee3ed6bf084abb40758cb39ac9c15bf222671
Ruslan Abdullin <ruslabd2003@gmail.com>
2024-04-02 21:36:37 +0700

    first commit

---
+++ backend/backend/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.template import loader
+from bdapp.models import sequence_desc
+from django.shortcuts import render
+from bdapp.models import interpretation
+from bdapp.models import sequence_tb 
+
+def show(request):
+    template = loader.get_template('index.html')
+    context = {}
+    rendered_page = template.render(context, request)
+    return HttpResponse(rendered_page)
+
+def search1(request):
+    news = sequence_desc.objects.all()
+    return render (request, 'index.html', {'news':news})
+
+def search(request):
+    oeis_id = request.GET.get('oeis_id')  
+    if oeis_id:
+        news = sequence_desc.objects.filter(OEIS_ID=oeis_id)
+    else:
+        return HttpResponse('Error')
+    # return HttpResponse(sequence_desc.objects.filter(OEIS_ID=str(oeis_id)))
+    return render(request, 'index.html', {'news': news})
---
+++ backend/bdapp/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+class sequence_desc(models.Model):
+    M_ID = models.AutoField(primary_key=True)
+    OEIS_ID = models.CharField(max_length=255, unique=True)
+    special_title = models.TextField()
+    number_of_parameters = models.IntegerField()
+    recurrent_formula = models.TextField(blank=True, null=True)
+    explicit_formula = models.TextField(blank=True, null=True)
+    other_formula = models.TextField(blank=True, null=True)
+    explicit_formula_latex = models.TextField(blank=True, null=True)
+    other_formula_latex = models.TextField(blank=True, null=True)
+    recurrent_formula_latex = models.TextField(blank=True, null=True)
+    generating_function = models.TextField(blank=True, null=True)
+    generating_function_latex = models.TextField(blank=True, null=True)
+
+class interpretation(models.Model):
+    Interp_ID = models.AutoField(primary_key=True)
+    n_value = models.TextField()
+    description = models.TextField()
+    example_text = models.TextField(blank=True, null=True)
+    example_image = models.ImageField(upload_to='images/')
+    example_table = models.TextField(blank=True, null=True)
+
+class algorithm(models.Model):
+    Alg_ID = models.AutoField(primary_key=True)
+    alg_name = models.TextField()
+    title = models.TextField()
+    number_of_parameters = models.IntegerField()
+    parameters_name = models.TextField()
+    capacity_formula = models.TextField(blank=True, null=True)
+    capacity_formula_latex = models.TextField(blank=True, null=True)
+    tree_structure = models.ImageField(upload_to='images/')
+    pseudocode = models.TextField(blank=True, null=True)
+    algorithm_code = models.TextField(blank=True, null=True)
+    href_code = models.TextField(blank=True, null=True)
+
+class sequence_tb(models.Model):
+    M_ID = models.ForeignKey(sequence_desc, on_delete=models.CASCADE)
+    Alg_ID = models.ForeignKey(algorithm, on_delete=models.CASCADE)
+    Interp_ID = models.ForeignKey(interpretation, on_delete=models.CASCADE)
