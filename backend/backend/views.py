0a4d4d3a8e117f55107a657a2f5a68bcbe14ce19
Ruslan Abdullin <ruslabd2003@gmail.com>
2024-04-03 23:47:26 +0700

    чуть переделапла seacrh, но нуждается в доработке

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
-from bdapp.models import sequence_desc
 from django.shortcuts import render
+
+
+from bdapp.models import sequence_desc
 from bdapp.models import interpretation
 from bdapp.models import sequence_tb 
+
 
 def show(request):
     template = loader.get_template('index.html')
@@ -15,7 +18,13 @@ def search(request):
     oeis_id = request.GET.get('oeis_id')  
     if oeis_id:
         news = sequence_desc.objects.filter(OEIS_ID=oeis_id)
+        if news:
+            m_id = news[0].M_ID
+            sequence_tb_object_count = sequence_tb.objects.filter(M_ID=m_id).count()
+            interpretation_modele= sequence_tb.objects.filter(M_ID=m_id)
+            return HttpResponse(interpretation_modele[0].Interp_ID.example_image)
+        else:
+            return HttpResponse('Error: OEIS_ID not found')
+
     else:
         return HttpResponse('Error')
-    # return HttpResponse(sequence_desc.objects.filter(OEIS_ID=str(oeis_id)))
-    return render(request, 'index.html', {'news': news})
