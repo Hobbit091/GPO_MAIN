d95e7c090d4a6f66b1cb40ea0c87f6e4665bd59d
Ruslan <ruslabd2003@gmail.com.>
2024-04-18 12:05:00 +0700

    Last One

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.show),
-    path('search',views.search),
-    path('search1',views.search1),
+    path('search',views.search_sequence),
+    path('search_interp',views.search_InterpSelect),
     path('solve',views.solve),
 ]
+    
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -5,76 +5,72 @@ import psycopg2
 import json
 
 from bdapp.models import sequence_desc
-from bdapp.models import interpretation
+from bdapp.models import interpretation 
 from bdapp.models import sequence_tb 
 from bdapp.models import algorithm
 
-
-def show(request):
+def show(request): #Заглушка, чтобы загружать стартову страницу
     template = loader.get_template('index.html')
     context = {}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
 
-
-def search1(request):
-    oeis_id = request.GET.get('oeis_id')  
+def search_sequence(request): # Вывести инфу о определенной последовательности
+    oeis_id = request.GET.get('oeis_id')
     if oeis_id:
         news = sequence_desc.objects.filter(OEIS_ID=oeis_id)
+        if news:
+            response = HttpResponse(news)
+            return response
+        else:
+            return HttpResponse('Error: OEIS_ID not found')
     else:
         return HttpResponse('Error')
-    return render(request, 'index.html', {'news': news})
 
-def search(request):
-    n = 5  # Общее количество элементов
-    k = 3  # Размер комбинации
+def search_InterpSelect(request): #Получить на сколько интерпретаций селекттор и сами интепретации, чтобы вставить в селектор. На выход идет лист, из него обращаться 
+    list_interp=[]
     oeis_id = request.GET.get('oeis_id')
     if oeis_id:
         news = sequence_desc.objects.filter(OEIS_ID=oeis_id)
         if news:
             m_id = news[0].M_ID
             sequence_tb_object_count = sequence_tb.objects.filter(M_ID=m_id).count()
-            modele= sequence_tb.objects.filter(M_ID=m_id)
-            # return HttpResponse(interpretation_modele[0].Interp_ID.example_image_process, content_type = 'image/jpg') вернуть изображение 
-            result=modele[0].Alg_ID.algorithm_code
-            n = 5  # Общее количество элементов
-            k = 3  # Размер комбинации
-            result=exec(result, globals())
-            combination_generator = CombinationColex(n, k)
-            res=combination_generator.Start()
-            response = HttpResponse(res[0])
+            interpretation_modele= sequence_tb.objects.filter(M_ID=m_id)
+            for i in range(0,sequence_tb_object_count):
+                list_interp.append(interpretation_modele[i].Interp_ID)
+            response = HttpResponse(list_interp)
             return response
-            #return HttpResponse(modele[0].Interp_ID.example_image_process, content_type = 'image/jpg') 
-            #return HttpResponse(modele[0].Alg_ID.algorithm_code)
-            #return render(request, 'index.html', {'object': modele[0]})
         else:
             return HttpResponse('Error: OEIS_ID not found')
     else:
         return HttpResponse('Error')
 
-def solve(request):
+
+def solve(request): #Выполнить код который хранится в бд
     oeis_id = request.GET.get('oeis_id')
-    news = sequence_desc.objects.filter(OEIS_ID=oeis_id)
-    m_id = news[0].M_ID
-    sequence_tb_object_count = sequence_tb.objects.filter(M_ID=m_id).count()
-    modele= sequence_tb.objects.filter(M_ID=m_id)
-    # return HttpResponse(interpretation_modele[0].Interp_ID.example_image_process, content_type = 'image/jpg') вернуть изображение 
-    result=modele[0].Alg_ID.algorithm_code
-    number_of_params = modele[0].Alg_ID.number_of_parameters
-    n = 5 
-    k = 3  
-    result=exec(result, globals())
-    #combination_generator = CombinationColex(n, k)
-    #resp = CombinationColex()
-    if number_of_params == 1:
-        res = resp.Start(n)
+    if oeis_id:
+        news = sequence_desc.objects.filter(OEIS_ID=oeis_id)
+        if news:
+            m_id = news[0].M_ID
+            modele= sequence_tb.objects.filter(M_ID=m_id)
+            result=modele[0].Alg_ID.algorithm_code
+            number_of_params = modele[0].Alg_ID.number_of_parameters
+            n = 5 #Сюда вбить параметры с формы, смотря сколько их
+            k = 3  
+            result=exec(result, globals())
+            if number_of_params == 1:
+                res = resp.Start(n)
+                
+            if number_of_params == 2:
+                res = resp.Start(n,k)
+            
+            if number_of_params == 3:
+                res = resp.Start(n,k,m)
+            
+            response = HttpResponse(res)
+            return response
+        else:
+            return HttpResponse('Error: OEIS_ID not found')
+    else:
+        return HttpResponse('Error')
         
-    if number_of_params == 2:
-        res = resp.Start(n,k)
-      
-    if number_of_params == 3:
-        res = resp.Start(n,k,m)
-    
-    response = HttpResponse(res)
-    return response
-    
